044b2c484878cfe49e77f857e0b2b41778b00d80
ChanderG <mail@chandergovind.org>
2025-05-08 10:24:14 +0530

    add basic working version

---
+++ ragman/core.py
@@ -0,0 +1,19 @@
+import chromadb
+import subprocess
+import tempfile
+
+def index_page(collection, page):
+    print(f"Indexing {page}...")
+
+    with tempfile.NamedTemporaryFile(mode="w+") as fp:
+        fname = fp.name
+        command = f"man -P cat {page} | col -b > {fname}"
+
+        subprocess.run(command, shell=True)
+
+        data = fp.read()
+        paras = data.split("\n\n")
+        print("Obtained ", len(paras), " paras...")
+
+    ids = list(map(lambda x: page+str(x), list(range(len(paras)))))
+    collection.add(documents=paras, ids=ids)
--- ragman/ragman.py
+++ ragman/ragman.py
@@ -1,4 +1,8 @@
 import click
+import os
+import chromadb
+
+from .core import index_page
 
 @click.group()
 def cli():
@@ -8,4 +12,22 @@ def cli():
 @click.argument("page")
 @click.argument("query")
 def search(page, query):
-    print(page, query)
+
+    # check for the db
+    cl = chromadb.PersistentClient(path=os.path.expanduser("~/.config/ragman/db.chroma"))
+
+    try:
+        col = cl.get_collection(name="man-"+page)
+    except chromadb.errors.NotFoundError as e:
+        col = cl.create_collection(name="man-"+page)
+        index_page(col, page)
+
+    res = col.query(query_texts=[query], n_results = 3)
+
+    # since query is an array of strings
+    # response too has a 2d array
+
+    print(10*"====")
+    for entry in res['documents'][0]:
+        print(entry)
+        print("\n" + 10*"====")
