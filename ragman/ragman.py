4aa99ab2925b42b07d823034f87d3ad46974aeb3
ChanderG <mail@chandergovind.org>
2025-05-09 09:30:05 +0530

    add chat command

--- ragman/ragman.py
+++ ragman/ragman.py
@@ -1,6 +1,7 @@
 import click
 import os
 import chromadb
+from openai import OpenAI
 
 from .core import index_page
 
@@ -8,11 +9,7 @@ from .core import index_page
 def cli():
     pass
 
-@cli.command()
-@click.argument("page")
-@click.argument("query")
-def search(page, query):
-
+def get_collection(page):
     # check for the db
     cl = chromadb.PersistentClient(path=os.path.expanduser("~/.config/ragman/db.chroma"))
 
@@ -22,6 +19,13 @@ def search(page, query):
         col = cl.create_collection(name="man-"+page)
         index_page(col, page)
 
+    return col
+
+@cli.command()
+@click.argument("page")
+@click.argument("query")
+def search(page, query):
+    col = get_collection(page)
     res = col.query(query_texts=[query], n_results = 3)
 
     # since query is an array of strings
@@ -31,3 +35,24 @@ def search(page, query):
     for entry in res['documents'][0]:
         print(entry)
         print("\n" + 10*"====")
+
+@cli.command()
+@click.argument("page")
+@click.argument("query")
+def chat(page, query):
+    col = get_collection(page)
+    res = col.query(query_texts=[query], n_results = 3)
+
+    msg = f"Given the following context, answer the final question for the command {page}."
+    for entry in res['documents'][0]:
+        msg += entry
+        msg += "\n"
+    msg += query
+
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="ibm-granite/granite-3.1-8b-instruct",
+        messages=[{"role": "user", "content": msg}])
+
+    print(completion.choices[0].message.content)
